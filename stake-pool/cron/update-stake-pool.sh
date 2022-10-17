e33ea15e730157c04a703bfb56a9620f076ea6f6
Bogdan Shumygora <b.shumygora@everstake.one>
2022-10-17 16:21:05 +0300

    fix small issue in the update script - 3

--- stake-pool/cron/update-stake-pool.sh
+++ stake-pool/cron/update-stake-pool.sh
@@ -2,5 +2,5 @@
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update w55eR1rNUjD1hLxvxsVtBDm98iPdSWTbLmTh8B1WA4k >> /home/stakepool/cron/update-stake-pool-test.log "$@" >> /home/stakepool/cron/update-stake-pool-test.log 2>> /home/stakepool/cron/update-stake-pool-test.log
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update EEx8JxE1hhpnQw1fLwMY1aLsYdCcphZyrYC3SLSjvx5R >> /home/stakepool/cron/update-stake-pool-test.log "$@" >> /home/stakepool/cron/update-stake-pool-test.log 2>> /home/stakepool/cron/update-stake-pool-test.log
 solana config set --url devnet
-/home/stakepool/solana-program-library/target/release/spl-stake-pool update EEx8JxE1hhpnQw1fLwMY1aLsYdCcphZyrYC3SLSjvx5R >> /home/stakepool/cron/update-stake-pool-dev.log "$@" >> /home/stakepool/cron/update-stake-pool-dev.log 2>> /home/stakepool/cron/update-stake-pool-dev.log
+/home/stakepool/solana-program-library/target/release/spl-stake-pool update J3WCjeAhgzH4DkN2bSLSAC7K1KU5r36ooBJkSMARrNjU >> /home/stakepool/cron/update-stake-pool-dev.log "$@" >> /home/stakepool/cron/update-stake-pool-dev.log 2>> /home/stakepool/cron/update-stake-pool-dev.log
 solana config set --url testnet
