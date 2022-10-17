69ff8c59b1cdf2035caf480f0f5a3092b25955be
Bogdan Shumygora <b.shumygora@everstake.one>
2022-10-17 16:04:16 +0300

    update update

--- stake-pool/cron/update-stake-pool.sh
+++ stake-pool/cron/update-stake-pool.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update w55eR1rNUjD1hLxvxsVtBDm98iPdSWTbLmTh8B1WA4k >> /home/stakepool/cron/update-stake-pool.log "$@" >> /home/stakepool/cron/update-stake-pool.log 2>> /home/stakepool/cron/update-stake-pool.log
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update EEx8JxE1hhpnQw1fLwMY1aLsYdCcphZyrYC3SLSjvx5R >> /home/stakepool/cron/update-stake-pool.log "$@" >> /home/stakepool/cron/update-stake-pool.log 2>> /home/stakepool/cron/update-stake-pool.log
+solana config set --url devnet
+/home/stakepool/solana-program-library/target/release/spl-stake-pool update EEx8JxE1hhpnQw1fLwMY1aLsYdCcphZyrYC3SLSjvx5R >> /home/stakepool/cron/update-stake-pool.log "$@" >> /home/stakepool/cron/update-stake-pool.log 2>> /home/stakepool/cron/update-stake-pool.log
+solana confg set --url testnet
