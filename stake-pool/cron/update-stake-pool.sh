be3e7ed6f9247b513e2ad988d34f3a510f098c8a
Bogdan Shumygora <b.shumygora@everstake.one>
2022-10-21 10:24:06 +0300

    add sleep to make sure we switched to devnet

--- stake-pool/cron/update-stake-pool.sh
+++ stake-pool/cron/update-stake-pool.sh
@@ -2,5 +2,7 @@
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update w55eR1rNUjD1hLxvxsVtBDm98iPdSWTbLmTh8B1WA4k >> /home/stakepool/cron/update-stake-pool-test.log "$@" >> /home/stakepool/cron/update-stake-pool-test.log 2>> /home/stakepool/cron/update-stake-pool-test.log
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update EEx8JxE1hhpnQw1fLwMY1aLsYdCcphZyrYC3SLSjvx5R >> /home/stakepool/cron/update-stake-pool-test.log "$@" >> /home/stakepool/cron/update-stake-pool-test.log 2>> /home/stakepool/cron/update-stake-pool-test.log
 solana config set --url devnet
+sleep 2
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update J3WCjeAhgzH4DkN2bSLSAC7K1KU5r36ooBJkSMARrNjU >> /home/stakepool/cron/update-stake-pool-dev.log "$@" >> /home/stakepool/cron/update-stake-pool-dev.log 2>> /home/stakepool/cron/update-stake-pool-dev.log
+sleep 2
 solana config set --url testnet
