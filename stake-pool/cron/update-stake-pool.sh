8b434d09d7934a747b12f114c73da773f241a850
Bogdan Shumygora <b.shumygora@everstake.one>
2022-10-26 15:54:08 +0300

    Update the update script for devnet and testnet

--- stake-pool/cron/update-stake-pool.sh
+++ stake-pool/cron/update-stake-pool.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update w55eR1rNUjD1hLxvxsVtBDm98iPdSWTbLmTh8B1WA4k >> /home/stakepool/cron/update-stake-pool-test.log "$@" >> /home/stakepool/cron/update-stake-pool-test.log 2>> /home/stakepool/cron/update-stake-pool-test.log
+wait
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update EEx8JxE1hhpnQw1fLwMY1aLsYdCcphZyrYC3SLSjvx5R >> /home/stakepool/cron/update-stake-pool-test.log "$@" >> /home/stakepool/cron/update-stake-pool-test.log 2>> /home/stakepool/cron/update-stake-pool-test.log
+wait
 solana config set --url devnet
-sleep 2
+wait
 /home/stakepool/solana-program-library/target/release/spl-stake-pool update J3WCjeAhgzH4DkN2bSLSAC7K1KU5r36ooBJkSMARrNjU >> /home/stakepool/cron/update-stake-pool-dev.log "$@" >> /home/stakepool/cron/update-stake-pool-dev.log 2>> /home/stakepool/cron/update-stake-pool-dev.log
-sleep 2
+wait
 solana config set --url testnet
